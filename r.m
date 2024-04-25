6b131b680809bc4fa3802511c1de78af25d278eb
Btaype <90937585+btaype@users.noreply.github.com>
2024-04-25 02:36:02 -1200

    Add files via upload

---
+++ r.m
@@ -0,0 +1,125 @@
+function m=biseccion(f,a,b,t)
+  m1=(a + b)/2;
+  fprintf('m = %.6f\n',m1 );
+  while abs(b-a)>t
+    if f(a)*f(m1)<0;
+      b=m1;
+    else
+      a=m1;
+    endif
+    m1=(a + b)/2;
+    fprintf('m = %.6f\n',m1 );
+  endwhile
+  m=m1;
+endfunction
+
+
+
+
+function m = puntofalso(f, a, b, t)
+  m1 = (a*f(b) - b*f(a)) / (f(b) - f(a));
+  fprintf('m = %.6f\n', m1);
+  while abs(b - a) > t
+    if f(a) * f(m1) < 0
+      b = m1;
+    else
+      a = m1;
+    endif
+    m1 = (a*f(b) - b*f(a)) / (f(b) - f(a));
+    fprintf('m = %.6f\n', m1);
+  endwhile
+  m = m1;
+endfunction
+
+
+
+
+function m = newton(x,g,t)
+  x1=g(x)
+  fprintf('m = %.6f\n',x1);
+  while abs(x1-x)>=t
+    x=x1;
+    x1=g(x);
+    fprintf('m = %.6f\n',x1);
+  endwhile
+  m=x1;
+endfunction
+
+
+
+
+
+
+
+function m=secante(g,x,t)
+  h=t;
+  x1=g(x,h);
+  fprintf('m = %.8f\n',x1);
+  while abs(x1-x)>=t
+    x=x1;
+    x1=g(x,h);
+    fprintf('m = %.8f\n',x1);
+  endwhile
+  m=x1;
+endfunction
+
+
+
+
+function inversa1=inversa_matriz(a)
+  n=size(a,1);
+  ext=eye(n);
+  a=[a,ext];
+  for i=1:n
+    a(i,:)= a(i,:)/a(i,i);
+    for j=1:n
+       if i~=j
+        a(j,:) = a(j,:) - a(i,:)*a(j,i);
+       endif
+    endfor
+  endfor
+  inversa1=a(:,n+1:end)
+endfunction
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
